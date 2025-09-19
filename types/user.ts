d5cbc29e092ab969b7d9fcbd9182237de300e514
shivaraj110 <shivarajchandaragi9@gmail.com>
2025-09-19 20:36:24 +0530

    added types

---
+++ types/user.ts
@@ -0,0 +1,11 @@
+import z from "zod";
+export const signUpSchema = z.object({
+	name: z.string(),
+	email: z.email(),
+	password: z.string().min(8),
+});
+
+export const loginSchema = z.object({
+	email: z.email(),
+	password: z.string().min(8),
+});
