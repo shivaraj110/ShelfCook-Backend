e011add7ab4ad4951be19d4b10bb0fe6787a41fc
shivaraj110 <shivarajchandaragi9@gmail.com>
2025-11-17 08:20:46 +0530

    endpoint for bulk adding vegetables

--- src/api/v1/ingredients.ts
+++ src/api/v1/ingredients.ts
@@ -19,8 +19,42 @@ app.post("/vegetable", verifyAuthToken, async (req: Request, res: Response) => {
   }
   res.status(201).json({
     status: "success",
-    message: "Ingredient added successfully!",
+    message: "vegetable added successfully!",
   });
 });
+
+app.post(
+  "/vegetables",
+  verifyAuthToken,
+  async (req: Request, res: Response) => {
+    const vegetableIngredients = req.body;
+    const ingredients = await prisma.vegetable.createMany({
+      data: vegetableIngredients,
+    });
+    if (!ingredients) {
+      return res.status(400).json({ message: "Failed to create ingredient" });
+    }
+    res.status(201).json({
+      status: "success",
+      message: "veggies added successfully!",
+    });
+  },
+);
+
+app.get("/vegetables", async (req: Request, res: Response) => {
+  const vegetables = await prisma.vegetable.findMany({
+    where: {
+      userIngredientId: req.body.userIngredientId,
+    },
+  });
+  if (!vegetables) {
+    return res.status(400).json({ message: "Failed to get ingredients" });
+  }
+  res.status(200).json({
+    vegetables,
+  });
+});
+
+app.post("");
 
 export default app;
